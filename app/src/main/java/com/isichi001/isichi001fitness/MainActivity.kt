931ceea982632d92de29273173a9d90cfbd9576d
Laurreine <laurreinisichi@gmail.com>
2025-10-02 13:37:55 +1000

    Initial commit

---
+++ app/src/main/java/com/isichi001/isichi001fitness/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.isichi001.isichi001fitness
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.isichi001.isichi001fitness.ui.theme.Isichi001FitnessTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       enableEdgeToEdge()
+        setContent {
+            Isichi001FitnessTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Laurreine",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Yellow) {
+        Text(
+            text = "Hello, my name is $name!",
+            modifier = modifier .padding(24.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Isichi001FitnessTheme {
+        Greeting("Laurreine")
+    }
+}
